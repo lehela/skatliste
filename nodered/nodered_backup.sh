03bdc3e1a3e5ce3e69528a5833093ff6b4842509
lehela <leif.lange@gmail.com>
2022-04-18 06:07:23 +0800

    (fix) root to exec backup

--- nodered/nodered_backup.sh
+++ nodered/nodered_backup.sh
@@ -10,7 +10,7 @@ BACKUPDIR=/host/backups
 ARCHIVE=node-red-skat.tgz
 
 # Write app files to archive
-docker exec $CONTAINER /bin/bash -c "\
+docker exec -u root $CONTAINER /bin/bash -c "\
     cd ""$USERDIR""; \
     tar -czf ""$BACKUPDIR""/""$ARCHIVE"" \
         flows.json \
